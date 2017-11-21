e89aaac489593618ce56eacdfc8643d733a930d2
wangqingshun <2184801857@qq.com>
2017-11-21 19:38:17 +0800

    添加js

---
+++ web1-8/web1-8.js
@@ -0,0 +1,49 @@
+/*
+* @Author: Marte
+* @Date:   2017-11-21 19:30:13
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-11-21 19:36:02
+*/
+var imag = document.getElementsByClassName("image");
+                imag[0].onmouseover = function(){
+                    imag[0].children[5].className = "b3";
+                }
+                imag[0].onmouseout = function(){
+                   imag[0].children[5].className = "b4";
+                }
+                imag[1].onmouseover = function(){
+                    imag[1].children[4].className = "b3";
+                }
+                imag[1].onmouseout = function(){
+                   imag[1].children[4].className = "b4";
+                }
+                imag[2].onmouseover = function(){
+                    imag[2].children[5].className = "b3";
+                }
+                imag[2].onmouseout = function(){
+                   imag[2].children[5].className = "b4";
+                }
+                imag[3].onmouseover = function(){
+                    imag[3].children[4].className = "b3";
+                }
+                imag[3].onmouseout = function(){
+                   imag[3].children[4].className = "b4";
+                }
+                imag[4].onmouseover = function(){
+                    imag[4].children[4].className = "b3";
+                }
+                imag[4].onmouseout = function(){
+                   imag[4].children[4].className = "b4";
+                }
+                imag[5].onmouseover = function(){
+                    imag[5].children[5].className = "b3";
+                }
+                imag[5].onmouseout = function(){
+                   imag[5].children[5].className = "b4";
+                }
+                imag[6].onmouseover = function(){
+                    imag[6].children[4].className = "b3";
+                }
+                imag[6].onmouseout = function(){
+                   imag[6].children[4].className = "b4";
+                }
